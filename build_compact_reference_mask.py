f601fb514984b64a4cebe711e53e0d4f68b4873b
brunoob <degomme.bruno@gmail.com>
2019-01-09 17:10:51 +0100

    needleman wunsch successfullgit add --all

--- build_compact_reference_mask.py
+++ build_compact_reference_mask.py
@@ -2,10 +2,9 @@ import re
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
-# import needleman_wunsch3 as nw
-from skbio.alignment import global_pairwise_align, TabularMSA
-import string
-from custom_sequence import CustomSequence
+from skbio.alignment import global_pairwise_align
+from custom_sequence import CustomSequence, subst_matrix
+
 
 # return array containing reference format corresponding to each ref (ref with digits replaced by 0 and _..._IS removed)
 def to_reference_format(refs):
@@ -19,7 +18,6 @@ def to_reference_format(refs):
                                                            re.sub('_Hearing_IS$', '',
                                                                   re.sub('[0-9]', '0',r))))))
     return reference_formats
-
 
 
 def build_masks(refs):
@@ -39,51 +37,12 @@ def build_masks(refs):
 
 
 
-
-
-
-
-
-# def align_masks(masks):
-
-subst_matrix = {}
-for x1 in string.ascii_lowercase+string.ascii_uppercase:
-    subst_matrix[x1]={}
-    for x2 in string.ascii_lowercase+string.ascii_uppercase:
-        if x1 == x2:
-            subst_matrix[x1][x2] = 1
-        else:
-            subst_matrix[x1][x2] = -1
-    for x2 in "0":
-        subst_matrix[x1][x2] = -100
-    for x2 in "-_/()":
-        subst_matrix[x1][x2] = -100
-for x1 in "0":
-    subst_matrix[x1]={}
-    for x2 in string.ascii_lowercase+string.ascii_uppercase:
-        subst_matrix[x1][x2] = -100
-    for x2 in "0":
-        subst_matrix[x1][x2] = 1
-    for x2 in "-_/()":
-        subst_matrix[x1][x2] = -100
-for x1 in "-_/()":
-    subst_matrix[x1]={}
-    for x2 in string.ascii_lowercase+string.ascii_uppercase:
-        subst_matrix[x1][x2] = -100
-    for x2 in "0":
-        subst_matrix[x1][x2] = -100
-    for x2 in "-_/()":
-        if x1 == x2:
-            subst_matrix[x1][x2] = 1
-        else:
-            subst_matrix[x1][x2] = -100
-
-
-def build_global_mask(refs):
+def build_compact_global_mask(refs):
     masks = build_masks(refs)
-    print(global_pairwise_align(CustomSequence(masks[0]), CustomSequence(masks[1]), gap_open_penalty=1, gap_extend_penalty=0, substitution_matrix=subst_matrix))
+    alignment = global_pairwise_align(CustomSequence(masks[0]), CustomSequence(masks[1]), gap_open_penalty=1, gap_extend_penalty=0, substitution_matrix=subst_matrix)
+    print(alignment)
     global_mask = '^(?i)(' + '|'.join(masks) + ')|(_Hearing_IS|_Complaint_IS|_Settlement_IS|_Verdict_IS|_Withdrawal_IS)\\?$'
-    return global_mask
+    return global_mask, alignment
 
 
 
@@ -99,19 +58,19 @@ def build_court_masks(court_id):
     result = pd.read_sql(query, conn)
 
     decision_refs = result["decision_ref"]
-    docket_refs = result["docket_ref"]
+    # docket_refs = result["docket_ref"]
 
-    decision_ref_mask = build_global_mask(decision_refs)
-    docket_ref_mask = build_global_mask(docket_refs)
+    (decision_ref_mask, alignment) = build_compact_global_mask(decision_refs)
+    # docket_ref_mask = build_global_mask(docket_refs)
 
-    return (decision_ref_mask, docket_ref_mask)
+    return (decision_ref_mask, docket_ref_mask, alignment)
 
 
 
 
 
 # PRV : 285
-(decision_ref_mask, docket_ref_mask) = build_court_masks(285)
+(decision_ref_mask, docket_ref_mask, alignment) = build_court_masks(285)
 
 
 print(len(decision_ref_mask))
--- custom_sequence.py
+++ custom_sequence.py
@@ -3,6 +3,40 @@ from skbio.util import classproperty
 import string
 
 
+subst_matrix = {}
+for x1 in string.ascii_lowercase+string.ascii_uppercase:
+    subst_matrix[x1]={}
+    for x2 in string.ascii_lowercase+string.ascii_uppercase:
+        if x1 == x2:
+            subst_matrix[x1][x2] = 1
+        else:
+            subst_matrix[x1][x2] = -1
+    for x2 in "0":
+        subst_matrix[x1][x2] = -100
+    for x2 in "-_/()":
+        subst_matrix[x1][x2] = -100
+for x1 in "0":
+    subst_matrix[x1]={}
+    for x2 in string.ascii_lowercase+string.ascii_uppercase:
+        subst_matrix[x1][x2] = -100
+    for x2 in "0":
+        subst_matrix[x1][x2] = 1
+    for x2 in "-_/()":
+        subst_matrix[x1][x2] = -100
+for x1 in "-_/()":
+    subst_matrix[x1]={}
+    for x2 in string.ascii_lowercase+string.ascii_uppercase:
+        subst_matrix[x1][x2] = -100
+    for x2 in "0":
+        subst_matrix[x1][x2] = -100
+    for x2 in "-_/()":
+        if x1 == x2:
+            subst_matrix[x1][x2] = 1
+        else:
+            subst_matrix[x1][x2] = -100
+
+
+
 class CustomSequence(GrammaredSequence):
     @classproperty
     def degenerate_map(cls):
